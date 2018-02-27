153c946165d866b619098a715ffb373a6c9c82b1
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-02-27 22:20:50 +0200

    add api gateway

---
+++ api-gateway/src/main/java/com/project/warehouse/WarehouseApplication.java
@@ -0,0 +1,18 @@
+package com.project.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableHystrix
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class WarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseApplication.class, args);
+	}
+}
