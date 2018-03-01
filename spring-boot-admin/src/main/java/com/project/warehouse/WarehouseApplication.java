45224a912d652ab312088f57b390a61c2f0d4883
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-03-01 20:36:27 +0200

    update sprig boot admin

--- spring-boot-admin/src/main/java/com/project/warehouse/WarehouseApplication.java
+++ spring-boot-admin/src/main/java/com/project/warehouse/WarehouseApplication.java
@@ -1,8 +1,12 @@
 package com.project.warehouse;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WarehouseApplication {
 
