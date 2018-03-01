ee06deb80e22ab3a066eca366c5ef4538ab1660e
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-03-01 20:26:48 +0200

    update turbine server

--- turbine-server/src/main/java/com/project/warehouse/WarehouseApplication.java
+++ turbine-server/src/main/java/com/project/warehouse/WarehouseApplication.java
@@ -3,6 +3,15 @@ package com.project.warehouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+//@EnableEurekaClient
+//@EnableTurbineStream
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class WarehouseApplication {
 
