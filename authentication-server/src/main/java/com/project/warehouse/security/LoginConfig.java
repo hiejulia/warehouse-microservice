cbccd86e56db6c3e52ce529e03707bf7f6a1b1b1
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-03-02 22:09:52 +0200

    update dependencies

---
+++ authentication-server/src/main/java/com/project/warehouse/security/LoginConfig.java
@@ -0,0 +1,34 @@
+package com.project.warehouse.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@Order(-20)
+public class LoginConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .requestMatchers().antMatchers("/account/userInfo","/login", "/oauth/authorize", "/oauth/confirm_access")
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.parentAuthenticationManager(authenticationManager);
+    }
+}
