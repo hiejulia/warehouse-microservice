70670264c3de34511e9d7740d17eba91b29323c7
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-03-02 23:19:45 +0200

    udpate

--- product-catalog-management-website/src/main/java/com/project/warehouse/WarehouseApplication.java
+++ product-catalog-management-website/src/main/java/com/project/warehouse/WarehouseApplication.java
@@ -3,6 +3,16 @@ package com.project.warehouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableHystrix
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class WarehouseApplication {
 
